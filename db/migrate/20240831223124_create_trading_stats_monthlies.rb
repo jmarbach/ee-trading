2cf44071b3d404733c6b2b5b85633bf74143645a
John Marbach <john.marbach@gmail.com>
2024-08-31 18:36:13 -0400

    continue prep for trading stats monthly records

---
+++ db/migrate/20240831223124_create_trading_stats_monthlies.rb
@@ -0,0 +1,29 @@
+class CreateTradingStatsMonthlies < ActiveRecord::Migration[7.0]
+  def change
+    create_table :trading_stats_monthlies do |t|
+      t.datetime :recorded_date
+      t.float :balance_btc
+      t.float :balance_btc_sats
+      t.float :balance_usd
+      t.float :balance_usd_cents
+      t.float :win_streak
+      t.float :lose_streak
+      t.float :draw_streak
+      t.float :last_12m_wins
+      t.float :last_12m_losses
+      t.float :last_12m_draws
+      t.float :net_balance_change_btc_absolute
+      t.float :net_balance_change_btc_sats_absolute
+      t.float :net_balance_change_usd_absolute
+      t.float :net_balance_change_usd_cents_absolute
+      t.float :net_balance_change_btc_percent
+      t.float :net_balance_change_usd_percent
+      t.boolean :win
+      t.boolean :loss
+      t.boolean :draw
+
+      t.datetime :created_at, default: ->{ 'now()' }
+      t.datetime :updated_at, default: ->{ 'now()' }
+    end
+  end
+end
